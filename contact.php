3a60b24b467fdc68dfbabcdaa2f2e5196c245597
tconsdarvin <88017912+tconsdarvin@users.noreply.github.com>
2021-07-27 10:29:28 +0530

    reffer

---
+++ contact.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="mail.php" method="post">
+        Name:
+        <input type="text" name="name"><br><br>
+        Mail id:
+        <input type="email" name="email"><br><br>
+        phone:
+        <input type="number" name="number"><br><br>
+        message:
+        <input type="text" name="message"><br><br>
+        <button>send</button>
+    </form>
+</body>
+</html>
